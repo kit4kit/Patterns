2033a04157d5c825d4a5aa5f9559dd7182ffc9b8
Nikita <trofimov1nv@gmail.com>
2021-02-08 22:31:59 +0300

    Edit text

--- src/main/java/ru/netology/DataGenerator.java
+++ src/main/java/ru/netology/DataGenerator.java
@@ -14,7 +14,8 @@ public class DataGenerator {
         }
         public  static RegistrationByCardInfo generateByCard(){
             Faker faker = new Faker(new Locale("ru"));
-            return new RegistrationByCardInfo(faker.address().city(), faker.name().fullName(), faker.phoneNumber().phoneNumber());
+            return new RegistrationByCardInfo(faker.address().city(), faker.name().fullName(),
+                    faker.phoneNumber().phoneNumber());
         }
         public static String getDayVisit(int plusDays){
             LocalDate today = LocalDate.now();
--- src/test/java/ru/netology/CardDeliveryNewFunctionalTest.java
+++ src/test/java/ru/netology/CardDeliveryNewFunctionalTest.java
@@ -27,13 +27,15 @@ public class CardDeliveryNewFunctionalTest {
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $(withText("Успешно!")).shouldBe(visible);
-        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на "+DataGenerator.Registration.getDayVisit(3)));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча " +
+                "успешно запланирована на "+DataGenerator.Registration.getDayVisit(3)));
         $("[data-test-id=date] input").sendKeys(Keys.CONTROL+"A"+Keys.DELETE);
         $("[data-test-id=date] input").setValue(DataGenerator.Registration.getDayVisit(7));
         $("button.button").click();
         $(withText("Необходимо подтверждение")).waitUntil(visible,15000);
         $("[data-test-id=replan-notification] .button").click();
         $(withText("Успешно!")).waitUntil(visible,15000);
-        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча успешно запланирована на "+DataGenerator.Registration.getDayVisit(7)));
+        $("[data-test-id=success-notification] .notification__content").shouldHave(exactText("Встреча " +
+                "успешно запланирована на "+DataGenerator.Registration.getDayVisit(7)));
     }
 }
